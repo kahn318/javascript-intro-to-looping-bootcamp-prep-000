318070870315014ef3e4f39a32b23482db10cca6
Kwang Hoon Christian Ahn <kwanghoonahn@gmail.com>
2017-03-30 01:44:08 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,7 +1,11 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
-    array.push('I am ${i} strange loop ${i===0 ? \'\' : \'s\' }.')
+     if ( i === 1) {
+     array.push("I am 1 strange loop.")
+     } else {
+    array.push(`I am ${i} strange loops.`)
   }
+}
   return array
 }
 
@@ -15,8 +19,11 @@ function whileLoop(n) {
 
 
 function doWhileLoop(array) {
+    function maybeTrue() {
+      return Math.random() >= 0.5
+    }
   do {
-    array.pop();
-    return array
+    array = array.slice(1);
   } while (array.length > 0 && maybeTrue());
+  return array
 }
